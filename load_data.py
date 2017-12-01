8f0530be3f398fdee417db5e97aeba49781aee4a
B. Bue <bbue@jpl.nasa.gov>
2017-12-01 12:57:41 -0800

    cleaner load_data with string load_func import

--- load_data.py
+++ load_data.py
@@ -1,41 +1,48 @@
 #!/usr/bin/env python
 from __future__ import division, print_function, absolute_import, unicode_literals
 
-from os.path import exists as pathexists
+from os.path import exists as pathexists, expanduser
 
 import numpy as np
-from skimage.io import ImageCollection, imread as skimread 
+from skimage.io import ImageCollection
 
 from tilepredictor_util import *
+from pylib import *
 
-def _load_path(labelf,nmax=np.inf,asarray=False,load_func=None,check_path=False,
-               conserve_memory=True,memory_slots=1,transpose=None,
-               exclude_pattern=None,mean_image=None,balance=False):
+require_load_func=True
 
-    load_func = load_func or skimread
-    
-    #exclude_pattern = '/tn/'
+
+def _parse_label_file(labelf,nmax=np.inf):
     imglabs = np.loadtxt(labelf,dtype=str)
-    n = imglabs.shape[0]
-    if nmax < n:
+    if nmax < imglabs.shape[0]:
         keepidx = np.random.permutation(n)[:nmax]
         imglabs = imglabs[keepidx]
-        n = nmax
-    elif nmax == np.inf:
-        nmax = n
-        
+
+    return imglabs
+
+def _load_path(labelf,load_func,nmax=np.inf,asarray=False,check_paths=False,
+               conserve_memory=True,memory_slots=1,transpose=None,balance=False,
+               class_mode='categorical',exclude_pattern=None,mean_image=None):
+
+    #exclude_pattern = '/tn/'
+    imglabs = _parse_label_file(labelf,nmax=nmax)
+    n = imglabs.shape[0]    
+
+    nmax = min(nmax,n)
     imgfiles,labs = [],[]
     nskip = 0
-    for i,(imgf,lab) in enumerate(imglabs):
+    pmsg = 'loading %d (of %d) images'%(nmax,len(imglabs))
+    pbar = progressbar(pmsg,n)
+    for i,(imgif,lab) in enumerate(pbar(imglabs)):
+        # paths can contain environment variables and "~"
+        imgf = os.path.expanduser(os.path.expandvars(imgif))
         if exclude_pattern and exclude_pattern in imgf:
             continue
-        if (i+1) % (n//10)==1:
-            print('loading image',i,'of',n,'(%d of %d selected)'%(len(labs),nmax))
 
         if i>=nmax:
             break
 
-        if check_path and not pathexists(imgf):
+        if check_paths and not pathexists(imgf):
             warn('image "%s" not found, skipping'%imgf)
             nskip+=1
             continue
@@ -50,18 +57,23 @@ def _load_path(labelf,nmax=np.inf,asarray=False,load_func=None,check_path=False,
         imgfiles = np.array(imgfiles)
         labs = np.array(labs)
         balance_idx = balance_classes(labs)
+        balance_labs = labs[balance_idx]
+        nlab = len(np.unique(labs))
+        nballab = len(np.unique(balance_labs))
+        labs = list(np.r_[labs,balance_labs])
         imgfiles = list(np.r_[imgfiles,imgfiles[balance_idx]])
-        nulab = len(np.unique(labs))
-        nbal = len(np.unique(labs[balance_idx]))
-        labs = list(np.r_[labs,labs[balance_idx]])
-        print('balanced %d of %d classes by sampling %d images with replacement'%(nbal,nulab,
-                                                                               len(balance_idx)))
+        print('balanced %d of %d classes by sampling %d images'
+              'with replacement'%(nballab,nlab,len(balance_idx)))
 
     imgs = ImageCollection(imgfiles,conserve_memory=conserve_memory,
                            load_func=load_func) #memory_slots=memory_slots
     if asarray:
         imgs = imgs.concatenate()
     labs = np.array(labs,dtype=np.int8)
+    if class_mode=='categorical':
+        labs = labs.squeeze()
+        labs = to_categorical(labs)
+        
     return imgs, labs
 
 def compute_mean(X_train,X_test,meanf):
@@ -74,24 +86,170 @@ def compute_mean(X_train,X_test,meanf):
     return mean_image
 
 def load_data(*args,**kwargs):
-    balance_train = kwargs.pop('balance_train',False)
     nargs = len(args)
-    X_train,y_train = [],[]
-    X_test,y_test = [],[]
-    if args[0]:
-        X_train,y_train = _load_path(args[0],balance=balance_train,**kwargs)
+    assert(nargs>=1)
+    if 'load_func' not in kwargs:
+        if require_load_func:
+            raise Exception('load_data requires load_func (e.g., skimage.io.imread) to load_data function!')
+        else:
+            print('load_func not specified, using skimage.io.imread as default reader')
+            from skimage.io import imread as _imread
+            kwargs['load_func'] = _imread
+
+    # only balance training set
+    balance_train = kwargs.pop('balance_train',False)
+    train_set = [_load_path(args[0],balance=balance_train,**kwargs)]
+
+    # test set(s)
+    test_sets = [_load_path(argi,**kwargs) for argi in args[1:]]
+                 
+    return train_set+test_sets
+
+def load_image_data(train_file,test_file,**kwargs):
+    load_func = kwargs.pop('load_func',None)
+    if load_func is None:
+        if require_load_func:
+            raise Exception('load_image_data requires load_func (e.g., skimage.io.imread) to load_data function!')
+        else:
+            print('load_func not specified, using skimage.io.imread as default reader')
+            from skimage.io import imread as _imread
+            load_func = _imread
+
+    balance_train = kwargs.pop('balance_train',False)
+    collect_test = kwargs.pop('collect_test',True)
+    test_percent = kwargs.pop('test_percent',0.2)
+    exclude_pattern = kwargs.pop('exclude_pattern',None)
+    mean_image = kwargs.pop('mean_image',None)
+    class_mode = kwargs.pop('class_mode','categorical')
+    if train_file==test_file:
+        warn('train_file==test_file, sampling test data from train_file')
+        test_file = None
+
+    (X_train,y_train),(X_test,y_test) = load_data(train_file,test_file,
+                                                  load_func=load_func,
+                                                  class_mode=class_mode,
+                                                  balance_train=False,
+                                                  exclude_pattern=exclude_pattern,
+                                                  mean_image=mean_image,
+                                                  **kwargs)
+    train_img_files = np.array(X_train.files)
+    if y_train.ndim==1 or y_train.shape[1]==1:
+        y_train = to_categorical(y_train)
+
+    train_lab = to_binary(y_train)
+    nb_classes = y_train.shape[1]
+
+    if len(y_test)==0:
+        msg='No test_file provided'
+        if test_percent > 0.0:
+            from sklearn.model_selection import train_test_split
+
+            msg+=', testing on %d%% of training data'%int(test_percent*100)
+            train_idx, test_idx = train_test_split(np.arange(y_train.shape[0]),
+                                                   train_size=1.0-test_percent,
+                                                   stratify=train_lab,
+                                                   random_state=random_state)
+
+            # remove any test samples present in the training data
+            test_idx = test_idx[~np.isin(train_img_files[test_idx],
+                                         train_img_files[train_idx])]
+            test_img_files = train_img_files[test_idx]
+            train_img_files = train_img_files[train_idx]
+            X_test = imgfiles2collection(test_img_files,load_func,**kwargs)
+            y_test,y_train = y_train[test_idx],y_train[train_idx]
+            train_lab = to_binary(y_train)
+            test_lab = to_binary(y_test)
+        else:
+            msg+=', test_percent=0, no validation will be performed'
+            X_test,y_test = [],[]
+            test_lab,test_img_files = [],[]
+        print(msg)
+    else:
+        test_img_files = np.array(X_test.files)
+        X_test = imgfiles2collection(test_img_files,load_func,**kwargs)
+        test_lab = to_binary(y_test)          
+        
+    if balance_train:
+        balance_idx = balance_classes(train_lab)
+        train_img_files = np.r_[train_img_files,train_img_files[balance_idx]]
+        y_train = np.r_[y_train,y_train[balance_idx]]
+        print('Balanced %d classes by sampling %d images with '
+              'replacement'%(nb_classes,len(balance_idx)))
+        train_lab = to_binary(y_train)
+        
+    X_train = imgfiles2collection(train_img_files,load_func,**kwargs)
     
-    if nargs>1 and args[1]:
-        X_test,y_test = _load_path(args[1],**kwargs)
-    return X_train,y_train,X_test,y_test
+    if len(y_test)!=0:
+        if y_test.ndim==1 or y_test.shape[1]==1:
+            y_test = to_categorical(y_test)
+        test_lab = to_binary(y_test)
+        if collect_test:
+            print('Collecting %d test samples'%len(y_test))
+            X_test = X_test.concatenate()
+            #X_test,y_test = collect_batch(X_test,y_test,verbose=1)
+
+
+    print("Training samples: {}, input shape: {}".format(len(X_train),X_train[0].shape))
+    print('Training classes:')
+    class_stats(train_lab,verbose=1)
+    print("Test samples: {}, shape: {}".format(len(X_test),X_test[0].shape))        
+    print('Test classes:')
+    class_stats(test_lab,verbose=1)
+    
+    return (X_train,y_train,train_img_files),(X_test,y_test,test_img_files)
+
+def imageccomp2tilegen(imagelistf,tile_dim,n_tiles,conserve_memory=True):
+
+    from skimage.transform import integral_image
+    from scipy.sparse import dok_matrix as spmat
+    detlist = np.loadtxt('detf.txt',dtype=str)
+    lablist = np.loadtxt('labf.txt',dtype=str)
+    imglist = np.c_[detlist,lablist]
+
+    #detfiles,ccompfiles,labfiles = imglist.T
+    from sklearn.feature_extraction.image import extract_patches_2d
+    tile_size = (tile_dim,tile_dim)
+    tile_off = tile_dim//2
+    for i,ifiles in enumerate(imglist):
+        detf,labf = map(expanduser,ifiles)
+        idet = np.uint8(imread(detf)[...,:3])
+        labs = imlabel(imread(labf)[...,:3].any(axis=2))
+
+        ipatch = np.dstack([idet,labs])
+
+        for lj in range(1,labs.max()):
+            (ib,il),(it,ir) = maskbbox(labs==lj,border=tile_off)
+            ntj = max(1,max(it-ib,ir-il)//tile_dim)
+            p = extract_patches_2d(ipatch[ib:it,il:ir], tile_size, ntj, random_state=42)        
+            pj,yj = p[...,:-1],p[...,-1].any(axis=-1)
+            for j in range(p.shape[0]):
+                pj,yj = p[j][...,:-1],p[j][...,-1].any()
+                nj+=1    
+            
+        p = extract_patches_2d(ipatch, tile_size, n_tiles, random_state=42)
+        pj,yj = p[...,:-1],p[...,-1].any(axis=-1)
+        nj=0
+        for j in range(p.shape[0]):            
+            pj,yj = p[j][...,:-1],p[j][...,-1].any()
+            if not pj.any():
+                continue
+            nj += 1
+            print('tile',nj,'#comp=',np.count_nonzero(pj.any(axis=2)),'lab=',yj)
+
+
+
 
 if __name__ == '__main__':
     import sys    
     import pylab as pl
+
+    tile_dim = 150
+    n_tiles = 50
+    imageccomp2tilegen('',tile_dim,n_tiles,conserve_memory=True)
+    raw_input()
     
     from tilepredictor import imread_tile
     from model_package import collect_batch, imaugment_perturb, batch_datagen_params 
-    #from keras.utils.np_utils import to_categorical 
     #from keras.preprocessing.image import ImageDataGenerator
     
     basepath=sys.argv[1]
@@ -193,8 +351,8 @@ if __name__ == '__main__':
     X_train_batch,y_train_batch = imaugment_perturb(X_batch,y_batch,
                                                 naugpos=naugpos,
                                                 naugneg=naugneg)
-    pos_idx,neg_idx = np.where(np.argmax(y_train_batch,-1)==1)[0],\
-                      np.where(np.argmax(y_train_batch,-1)==0)[0]
+    pos_idx,neg_idx = np.where(to_binary(y_train_batch)==1)[0],\
+                      np.where(to_binary(y_train_batch)==0)[0]
     print(y_train_batch.shape,len(pos_idx),len(neg_idx)),raw_input()
     
     fig,ax = pl.subplots(2,2,sharex=True,sharey=True)
