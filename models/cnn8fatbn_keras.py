60e0d9ca22039eb1d6200059596aae716ffbf28a
B. Bue <bbue@jpl.nasa.gov>
2018-03-02 11:54:15 -0800

    added a few additional model definition files that have been useful in practice

---
+++ models/cnn8fatbn_keras.py
@@ -0,0 +1,63 @@
+_model_id='CNN5BN'
+def model_init(input_shape,**kwargs):
+    from keras.models import Sequential
+    from keras.layers.core import Dense, Activation, Flatten, Dropout
+    from keras.layers.convolutional import Convolution2D, MaxPooling2D
+    from keras.layers.normalization import BatchNormalization
+    from keras.layers import GlobalAveragePooling2D
+    
+    assert(len(input_shape)==3)
+    
+    try:
+        from keras.layers.core import SpatialDropout2D
+    except:
+        from keras import __version__ as __kv__
+        from warnings import warn
+        warn('no SpatialDropout2D layer in keras version: %s'%__kv__)
+        SpatialDropout2D = Dropout
+    
+    # need to set the input_shape to first layer for a new model
+    model = Sequential()
+    model.add(Convolution2D(64,(3,3),padding='same',input_shape=input_shape))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(Convolution2D(64,(3,3)))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2,2)))
+    model.add(SpatialDropout2D(0.1))    
+    
+    model.add(Convolution2D(64,(2,2),padding='same'))
+    model.add(BatchNormalization())    
+    model.add(Activation('relu'))
+    model.add(Convolution2D(64,(2,2)))
+    model.add(BatchNormalization())    
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2,2)))
+    model.add(SpatialDropout2D(0.2))
+    
+    model.add(Convolution2D(72,(2,2),padding='same'))
+    model.add(BatchNormalization())    
+    model.add(Activation('relu'))
+    model.add(Convolution2D(72,(2,2)))
+    model.add(BatchNormalization())    
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2,2)))
+    model.add(SpatialDropout2D(0.3))
+
+    model.add(Convolution2D(96,(2,2),padding='same'))
+    model.add(BatchNormalization())    
+    model.add(Activation('relu'))
+    model.add(Convolution2D(96,(2,2)))
+    model.add(BatchNormalization())    
+    model.add(Activation('relu'))    
+    model.add(MaxPooling2D(pool_size=(2,2)))    
+    model.add(SpatialDropout2D(0.4))
+    
+    model.add(GlobalAveragePooling2D())
+    model.add(Dense(2048))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5))
+
+    return dict(model=model,lr_mult=1.0)
+
---
+++ models/xceptionretrain_keras.py
@@ -0,0 +1,45 @@
+from warnings import warn
+_model_id = 'Xception'
+MAX_BLOCK = 14 # xception max block=14
+def model_init(input_shape,**kwargs):
+    from keras.applications import Xception
+    from keras import backend
+
+    assert(backend.backend()=='tensorflow')
+    assert(len(input_shape)==3 and input_shape[2]==3)
+    backend.set_image_data_format('channels_last')
+
+    fix_base = False
+    if not fix_base:
+        warn('%s model fix_base=False, training may take a long time'%_model_id)
+    
+    max_block = kwargs.pop('max_block',MAX_BLOCK) 
+
+    base_model = Xception(input_shape=input_shape, weights="imagenet",
+                          include_top=False, pooling="avg")
+
+    n_layers = len(base_model.layers)
+    max_layer = kwargs.pop('max_layer',n_layers)
+    max_layer = min(n_layers,max_layer) if max_layer>0 else n_layers+max_layer
+
+    # pop any unwanted top layers
+    for i in range(n_layers-max_layer):
+        base_model.pop()
+    
+    if fix_base:
+        print('Fixing %s base_model layers'%_model_id)
+        # first: train only the top layers (which were randomly initialized)
+        trainable = False # fix blocks until we hit max_block
+        for i,layer in enumerate(base_model.layers):
+            lname = layer.name
+            if not trainable and max_block < MAX_BLOCK:
+                if lname.startswith('block'):                    
+                    spl = lname.split('_')
+                    lid = int(spl[0].replace('block',''))
+                    if lid>max_block:
+                        trainable = True
+                        base_model.layers[i-1].trainable = trainable
+                
+            layer.trainable = trainable
+        
+    return dict(model=base_model,lr_mult=0.25)
