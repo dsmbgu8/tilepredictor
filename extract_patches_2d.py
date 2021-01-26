fa3c9b0092747dc212044ceb08559946fcef9345
B. Bue <bbue@jpl.nasa.gov>
2021-01-26 15:07:13 -0800

    cosmetic updates, debug output for extract_patches

--- extract_patches_2d.py
+++ extract_patches_2d.py
@@ -51,7 +51,7 @@ def extract_patches(arr, patch_shape=8, extraction_step=1, writeable=False):
 
     patch_strides = arr.strides
 
-    slices = [slice(None, None, st) for st in extraction_step]
+    slices = tuple([slice(None, None, st) for st in extraction_step])
     indexing_strides = arr[slices].strides
 
     patch_indices_shape = ((np.array(arr.shape) - np.array(patch_shape)) //
@@ -64,7 +64,7 @@ def extract_patches(arr, patch_shape=8, extraction_step=1, writeable=False):
     return patches
 
 def extract_patches_2d(image, patch_size, max_patches=None, random_state=None,
-                       return_index=False, writeable=False, randomize=True):
+                       writeable=False, return_index=False, randomize=True):
     """
     Modified version of sklearn.feature_extraction.image.extract_patches_2d
     
@@ -152,8 +152,12 @@ def extract_patches_2d(image, patch_size, max_patches=None, random_state=None,
     image = image.reshape((i_h, i_w, -1))
     n_colors = image.shape[-1]
     patch_shape = (p_h,p_w,n_colors)
+    extraction_step = 1
+    
+    # always use extraction step = 1 here, subsample later with extraction_step
     extracted_patches = extract_patches(image, patch_shape=patch_shape,
-                                        extraction_step=1, writeable=False)
+                                        extraction_step=1,
+                                        writeable=False)
 
     n_patches = _compute_n_patches(i_h, i_w, p_h, p_w, max_patches)
     
@@ -161,46 +165,57 @@ def extract_patches_2d(image, patch_size, max_patches=None, random_state=None,
                   min(max_patches,n_patches)
     h_max = i_h - p_h + 1
     w_max = i_w - p_w + 1
+    i_s = np.arange(0, h_max, extraction_step, dtype=np.int)
+    j_s = np.arange(0, w_max, extraction_step, dtype=np.int)
+    
     if randomize:
         rng = check_random_state(random_state)
-        i_s = rng.randint(h_max, size=max_patches)
-        j_s = rng.randint(w_max, size=max_patches)
-        patches = extracted_patches[i_s, j_s, 0]
-    elif max_patches == n_patches:
-        patches = extracted_patches[:, :, 0]
-        if return_index:
-            i_s,j_s = map(np.ravel,np.meshgrid(np.arange(h_max, dtype=np.int),
-                                               np.arange(w_max, dtype=np.int)))
-
+        i_s = i_s[rng.randint(len(i_s), size=max_patches)]
+        j_s = j_s[rng.randint(len(j_s), size=max_patches)]
     else:
-        # return the first max_patches patches without randomization
-        i_s,j_s = map(np.ravel,np.meshgrid(np.arange(h_max, dtype=np.int),
-                                           np.arange(w_max, dtype=np.int)))
-        patches = extracted_patches[i_s[:max_patches],j_s[:max_patches], 0]
-    
+        i_s,j_s = map(np.ravel,np.meshgrid(i_s,j_s))
+        if max_patches != n_patches:
+            # return the first max_patches patches without randomization
+            i_s,j_s = i_s[:max_patches],j_s[:max_patches]
+            
+    patches = extracted_patches[i_s, j_s, 0]
     
     # remove the color dimension if useless
     if patches.shape[-1] == 1:
-        patches = patches.reshape(max_patches, p_h, p_w)
+        patches = patches.reshape(n_patches, p_h, p_w)
     else:
-        patches = patches.reshape(max_patches, p_h, p_w, n_colors)
+        patches = patches.reshape(n_patches, p_h, p_w, n_colors)
 
     if return_index:
         return patches, np.c_[i_s,j_s]
+    
     return outpatches
+
+def patch_counts(patch_index,patch_size):
+    p_h,p_w = patch_size
+    max_row,max_col = patch_index.max(axis=0)+patch_size
+
+    mask = np.zeros([max_row,max_col])
+    for c,(i,j) in enumerate(patch_index):
+        mask[i:i+p_h,j:j+p_w] += 1
+
+    return mask
 
 if __name__ == '__main__':
     # from extract_patches_2d import *
     # fix seed for reproducible output patches
     np.random.seed(42) 
-    input_image = np.arange(16).reshape((4, 4))
+    input_image = np.arange(36).reshape((6, 6))
     print('input_image')
     print(input_image)
     # array([[ 0,  1,  2,  3],
     #        [ 4,  5,  6,  7],
     #        [ 8,  9, 10, 11],
     #        [12, 13, 14, 15]])
-    patches,indices = extract_patches_2d(input_image, (2, 2), return_index=True)
+    patch_size=(2, 2)
+    patches,indices = extract_patches_2d(input_image, patch_size,
+                                         randomize=False,
+                                         return_index=True)
     print(patches.shape)
     # (9, 2, 2)
     print(indices.shape)
@@ -215,8 +230,16 @@ if __name__ == '__main__':
     print('indices[1]')
     print(indices[1])
 
-    print('patches[8]')
-    print(patches[8])
-    print('indices[8]')
-    print(indices[8])
+    print('patches[-2]')
+    print(patches[-2])
+    print('indices[-2]')
+    print(indices[-2])
 
+    print('patches[-1]')
+    print(patches[-1])
+    print('indices[-1]')
+    print(indices[-1])
+
+    mask = patch_mask(indices,patch_size)
+
+    print('mask: "%s"'%str((mask)))
